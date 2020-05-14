81fd4ae262c76c37025239738041ce9281325c66
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-14 07:32:36 +0530

    Update dice.py
    dart is added now

--- stdplugins/dice.py
+++ stdplugins/dice.py
@@ -1,7 +1,9 @@
 #bug fixed by @d3athwarrior
 
-"""@RollADie
-Syntax: .dice or .dice 1 to 6 any value """
+"""@RollADie with dash
+Syntax: .dice or .dice 1 to 6 any value 
+        .dart or .dart 1 to 6 any value
+        `you would be in trouble if you input any other value than mentioned.`"""
 from telethon.tl.types import InputMediaDice
 from uniborg.util import admin_cmd
 
@@ -21,3 +23,20 @@ async def _(event):
                 r = await event.reply(file=InputMediaDice(''))
         except:
             pass
+
+@borg.on(admin_cmd(pattern="dart ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('🎯'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('🎯'))
+        except:
+            pass
+        
